d8c41f00f000cb98e5df14a7fe845118b33793f8
Si Thu San <sithusanss@gmail.com>
2025-08-07 14:17:04 +0700

    feat: basic repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,29 @@
+import { stdin, stdout } from "process";
+import { createInterface } from "readline";
+
+const io = createInterface({
+    input: stdin,
+    output: stdin,
+    prompt: 'Pokedex > ',
+})
+
 export const cleanInput =
     (input: string): string[] => input.toLocaleLowerCase()
         .split(" ")
         .filter((word: string) => word.length > 0);
+
+export const startREPL = () => {
+    io.prompt();
+    io.on("line", (input: string) => {
+
+        if (input.length === 0) {
+            io.prompt();
+            return;
+        }
+
+        const cleanedInput = cleanInput(input);
+        console.log(`Your command was: ${cleanedInput.at(0)}`)
+
+        io.prompt();
+    });
+} 
